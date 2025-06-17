07c55e8b43b5794c4d9f49e8db144725566ec5a9
NotFaizen <munavir370@gmail.com>
2025-06-17 11:52:22 +0600

    changed bot status

--- Events/status.js
+++ Events/status.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'status',
     type: 'ready',
     code: `
-    $setStatus[do not disturb;Watching;Tengen Toppa Gurren Lagann]
+    $setStatus[do_not_disturb;Watching;Tengen Toppa Gurren Lagann]
     $chalkLog[[$day[TwoDigit]/$month[TwoDigit]/$year[TwoDigit] $hour[TwoDigit]:$minute[TwoDigit]:$second[TwoDigit]\\] Ready on client $userDisplayName[$clientID];green;bold]
     `
 }
